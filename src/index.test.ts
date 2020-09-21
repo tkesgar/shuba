40414c033b898395660b1b8231e772846aa41a64
Ted Kesgar <t.kesgar@live.com>
2020-09-22 05:29:07 +0700

    feat: change handle fn args, handle result by type
    object, number, and boolean uses res.json so that the content type is
    set to application/json, while string uses res.send to allow sending
    HTML strings.
    
    bigint, symbol, and function throws error (since they are not meant
    to be JSON-serializable).
    
    With context object, we can add request helpers in the future without
    having to modify the Express/Node req/res object.
    
    BREAKING CHANGE: handle is now incompatible with existing handle
    implementations, which put req and res as first and second args for the
    handler function.

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,3 +1,4 @@
+import { ErrorRequestHandler, RequestHandler } from "express";
 import { createTestResponse } from "@tkesgar/ariadoa";
 import handle from ".";
 
@@ -10,15 +11,154 @@ describe("handle", () => {
     expect(JSON.parse(payload)).toEqual({ foo: "bar" });
   });
 
-  it.todo("should continue to next middleware if headers has not been sent");
-  it.todo("should not continue to next middleware if headers has been sent");
+  it("should continue to next middleware if headers has not been sent", async () => {
+    const { payload } = await createTestResponse([
+      handle(({ res }) => {
+        res.locals.shuba = "shuba shuba shuba";
+      }),
+      ((req, res) => {
+        res.send(res.locals.shuba);
+      }) as RequestHandler,
+    ]);
+
+    expect(payload).toBe("shuba shuba shuba");
+  });
+
+  it("should not continue to next middleware if headers has been sent", async () => {
+    const { payload } = await createTestResponse([
+      handle(({ res }) => {
+        res.send("subaru wa warukunai yo");
+      }),
+      ((req, res) => {
+        res.send("watame wa warukunai yo");
+      }) as RequestHandler,
+    ]);
+
+    expect(payload).toBe("subaru wa warukunai yo");
+  });
 
   describe("with various return value types", () => {
-    it.todo("should return JSON object if type = 'object'");
-    it.todo("should return JSON number if type = 'number'");
-    it.todo("should return JSON string if type = 'string'");
-    it.todo("should throw error if type = 'bigint'");
-    it.todo("should throw error if type = 'symbol'");
-    it.todo("should throw error if type = 'function'");
+    it("should return JSON object if type = 'object'", async () => {
+      const response = await createTestResponse([
+        handle(() => {
+          return { foo: "bar" };
+        }),
+      ]);
+
+      expect(response.headers["content-type"]).toContain("application/json");
+      expect(JSON.parse(response.payload)).toEqual({ foo: "bar" });
+    });
+
+    it("should return JSON number if type = 'number'", async () => {
+      const response = await createTestResponse([
+        handle(() => {
+          return 123456;
+        }),
+      ]);
+
+      expect(response.headers["content-type"]).toContain("application/json");
+      expect(JSON.parse(response.payload)).toBe(123456);
+    });
+
+    it("should return JSON boolean if type = 'boolean'", async () => {
+      const response = await createTestResponse([
+        handle(() => {
+          return true;
+        }),
+      ]);
+
+      expect(response.headers["content-type"]).toContain("application/json");
+      expect(JSON.parse(response.payload)).toBe(true);
+    });
+
+    it("should return string if type = 'string'", async () => {
+      const response = await createTestResponse([
+        handle(() => {
+          return "<h1>Hello world!</h1><p>It works!</p>";
+        }),
+      ]);
+
+      expect(response.headers["content-type"]).toContain("text/html");
+      expect(response.payload).toBe("<h1>Hello world!</h1><p>It works!</p>");
+    });
+
+    it("should throw error if type = 'bigint'", async () => {
+      const response = await createTestResponse([
+        handle(() => {
+          return BigInt("0xffffffffffffffffffffffffffffffff");
+        }),
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        ((err, req, res, next) => {
+          res.send(err.message);
+        }) as ErrorRequestHandler,
+      ]);
+
+      expect(response.payload).toBe(
+        "Unsupported handle return value type: bigint"
+      );
+    });
+
+    it("should throw error if type = 'symbol'", async () => {
+      const response = await createTestResponse([
+        handle(() => {
+          return Symbol("SHUBA SHUBA SHUBA");
+        }),
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        ((err, req, res, next) => {
+          res.send(err.message);
+        }) as ErrorRequestHandler,
+      ]);
+
+      expect(response.payload).toBe(
+        "Unsupported handle return value type: symbol"
+      );
+    });
+
+    it("should throw error if type = 'function'", async () => {
+      const response = await createTestResponse([
+        handle(() => {
+          return function () {
+            return "foo";
+          };
+        }),
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        ((err, req, res, next) => {
+          res.send(err.message);
+        }) as ErrorRequestHandler,
+      ]);
+
+      expect(response.payload).toBe(
+        "Unsupported handle return value type: function"
+      );
+    });
+
+    it("should await for Promise and return the resolved value", async () => {
+      const response = await createTestResponse([
+        handle(() => {
+          return Promise.resolve({ foo: "bar" });
+        }),
+      ]);
+
+      expect(JSON.parse(response.payload)).toEqual({ foo: "bar" });
+    });
+
+    it("should await for Promise and throw the rejected error", async () => {
+      const expectedError = new Error("watame wa warukunai yo ne");
+      let actualError: typeof expectedError;
+
+      const response = await createTestResponse([
+        handle(() => {
+          return Promise.reject(expectedError);
+        }),
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        ((err, req, res, next) => {
+          actualError = expectedError;
+          res.send(err.message);
+        }) as ErrorRequestHandler,
+      ]);
+
+      expect(actualError).toEqual(expectedError);
+      expect(response.payload).toBe("watame wa warukunai yo ne");
+    });
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,40 @@
 import { Request, Response, RequestHandler } from "express";
 
-export type HandleFunction<T = void> = (
-  req: Request,
-  res: Response
-) => T | Promise<T>;
+export type HandleFunction<T = void> = (ctx: {
+  req: Request;
+  res: Response;
+}) => T | Promise<T>;
 
 export default function handle<T = void>(
   fn: HandleFunction<T>
 ): RequestHandler {
   return (req, res, next) =>
     (async () => {
-      const result = await fn(req, res);
+      const result = await fn({ req, res });
 
-      if (typeof result === "undefined") {
-        if (res.headersSent) {
-          return;
-        }
+      switch (typeof result) {
+        case "undefined":
+          if (res.headersSent) {
+            return;
+          }
 
-        next();
+          next();
+          break;
+        case "object":
+        case "boolean":
+        case "number":
+          res.json(result);
+          break;
+        case "bigint":
+        case "symbol":
+        case "function":
+          throw new Error(
+            `Unsupported handle return value type: ${typeof result}`
+          );
+        case "string":
+        default:
+          res.send(result);
+          break;
       }
-
-      res.send(result);
     })().catch(next);
 }
